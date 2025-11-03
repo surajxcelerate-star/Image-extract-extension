ec36fffd07b62a38d9d137b95e0a3e0fa28a8339
surajxcelerate-star <suraj.xcelerate@gmail.com>
2025-11-03 17:53:56 +0545

    Updated

---
+++ background.js
@@ -0,0 +1,11 @@
+// background service worker: injects the content script when the extension icon is clicked
+chrome.action.onClicked.addListener(async (tab) => {
+  try {
+    await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ["content.js"]
+    });
+  } catch (err) {
+    console.error("Failed to inject content script:", err);
+  }
+});
